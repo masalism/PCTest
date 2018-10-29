1f2274fcd411cf49504f3e44f87f8d5ffa783577
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2018-10-29 20:42:52 +0200

    1 part done

--- src/App.js
+++ src/App.js
@@ -21,12 +21,20 @@ class App extends Component {
     
     render() {
         if (this.state.posts.lenght === 0) {
-            return <h1>Loading</h1>
+            return <h1 className='text-danger'>Loading</h1>
         } else {
             return (
                 <div>
-                    <h1 className='text-center'>PCTest</h1>
-                    <PostList posts={this.state.posts} />
+                    <h1 className='text-center text-warning display-2 p-4'>PCTest</h1>
+                    <div className="container-fluid">
+                        <div className='row bg-warning text-dark p-1 text-center lead h1 align-items-center'>
+                            <div className='col-1 '>Post Number</div>
+                            <div className='col-1 '>User</div>
+                            <div className='col-5 '>Title</div>
+                            <div className='col-5 '>Body</div>
+                        </div>
+                        <PostList posts={this.state.posts} />
+                    </div>
                 </div>
             );
         }  
--- src/Post.js
+++ src/Post.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 const Post = ({ id, userId, title, body }) => {
     return (
-        <div className='text-center'>
-            <p>{id}</p>
-            <p>{userId}</p>
-            <h2>{title}</h2>
-            <p>{body}</p>
+        <div className='container-fluid'>
+            <div className='row text-center py-3 border-bottom'>
+                <div className='col-1'>{id}</div>
+                <div className='col-1'>{userId}</div>
+                <div className='col-5'>{title}</div>
+                <div className='col-5'>{body}</div>
+            </div>
         </div>
     );
 }
