5d8df223d0da3c2e2c50de57bb6a2a053dd64735
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2018-10-29 21:41:34 +0200

    before router

--- src/App.js
+++ src/App.js
@@ -28,10 +28,10 @@ class App extends Component {
                     <h1 className='text-center text-warning display-2 p-4'>PCTest</h1>
                     <div className="container-fluid">
                         <div className='row bg-warning text-dark p-1 text-center lead h1 align-items-center'>
-                            <div className='col-1 '>Post Number</div>
-                            <div className='col-1 '>User</div>
-                            <div className='col-5 '>Title</div>
-                            <div className='col-5 '>Body</div>
+                            <div className='col-1'>Post Number</div>
+                            <div className='col-1'>User</div>
+                            <div className='col-5'>Title</div>
+                            <div className='col-5'>Body</div>
                         </div>
                         <PostList posts={this.state.posts} />
                     </div>
--- src/Post.js
+++ src/Post.js
@@ -6,7 +6,7 @@ const Post = ({ id, userId, title, body }) => {
             <div className='row text-center py-3 border-bottom'>
                 <div className='col-1'>{id}</div>
                 <div className='col-1'>{userId}</div>
-                <div className='col-5'>{title}</div>
+                <div className='col-5'><a href={`https://jsonplaceholder.typicode.com/posts/${id}`}>{title}</a></div>
                 <div className='col-5'>{body}</div>
             </div>
         </div>
